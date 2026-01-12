bd640981a673678c194e88d2c86a8fe5a6854902
Isaac Ruiz <isaac.ruiz.us@gmail.com>
2026-01-12 14:29:33 -0800

    celadore vite mern init

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,11 +3,20 @@ import { devtools } from '@tanstack/devtools-vite'
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
 import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [devtools(), viteReact(), tailwindcss()],
+  plugins: [
+    devtools(),
+    tanstackRouter({
+      target: 'react',
+      autoCodeSplitting: true,
+    }),
+    viteReact(),
+    tailwindcss(),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
